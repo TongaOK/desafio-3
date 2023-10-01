458462eb22ed584e8e76a8152a94e41b181ae714
Ignacio Puglisi <puglisi.ignacio@gmail.com>
2023-09-30 22:43:25 -0300

    Entrega del desafio 3 del curso de Backend

---
+++ src/ProductManager.js
@@ -0,0 +1,190 @@
+const fs = require("fs");
+
+class ProductManager {
+  constructor(path) {
+    this.path = path;
+    this.productIdCounter = 1;
+  }
+
+  async addProduct(product) {
+    const { title, description, price, thumbnail, code, stock } = product;
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      throw new Error("Todos los campos son obligatorios.");
+    }
+    const products = await getJSONFromFile(this.path);
+    if (products.some((p) => p.code === code)) {
+      console.log(`Ya se encuentra agregado ese code: ${code}`);
+    } else {
+      const id = this.productIdCounter++;
+      const newProduct = {
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+        id,
+      };
+      products.push(newProduct);
+      return saveJSONToFile(this.path, products);
+    }
+  }
+
+  async getProducts() {
+    return await getJSONFromFile(this.path);
+  }
+
+  async getProductById(id) {
+    const products = await this.getProducts();
+    const productById = products.find((p) => p.id === id);
+    if (!productById) {
+      console.log("Product not found");
+    } else {
+      console.log("Product found", productById);
+    }
+  }
+
+  async updateProduct(
+    newTitle,
+    newDescription,
+    newPrice,
+    newThumbnail,
+    newCode,
+    newStock,
+    id
+  ) {
+    const products = await getJSONFromFile(this.path);
+    let ProdId = products.some((p) => p.id === id);
+    if (!ProdId) {
+      console.log(`updateProduct: Product not found, id: ${id}`);
+    } else {
+      const updatedProducts = products.map((p) => {
+        if (p.id === id) {
+          return {
+            title: newTitle,
+            description: newDescription,
+            price: newPrice,
+            thumbnail: newThumbnail,
+            code: newCode,
+            stock: newStock,
+            id: id,
+          };
+        }
+        return p;
+      });
+      await saveJSONToFile(this.path, updatedProducts);
+      console.log("Lista actualizada correctamente", updatedProducts);
+    }
+  }
+
+  async deleteProduct(id) {
+    const products = await this.getProducts();
+    let index = products.findIndex((p) => p.id === id);
+    if (index > -1) {
+      products.splice(index, 1);
+      await saveJSONToFile(this.path, products);
+      console.log("Se ha borrado correctamente el producto");
+    } else {
+      console.log("No se ha podido borrar el producto");
+    }
+  }
+}
+
+const existFile = async (path) => {
+  try {
+    await fs.promises.access(path);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const getJSONFromFile = async (path) => {
+  if (!(await existFile(path))) {
+    return [];
+  }
+
+  let content;
+
+  try {
+    content = await fs.promises.readFile(path, "utf-8");
+  } catch (error) {
+    throw new Error(`El archivo ${path} no pudo ser leído.`);
+  }
+
+  try {
+    return JSON.parse(content);
+  } catch (error) {
+    throw new Error(`El archivo ${path} no tiene un formato JSON válido.`);
+  }
+};
+
+const saveJSONToFile = async (path, data) => {
+  const content = JSON.stringify(data, null, "\t");
+  try {
+    await fs.promises.writeFile(path, content, "utf-8");
+  } catch (error) {
+    throw new Error(`El archivo ${path} no pudo ser escrito.`);
+  }
+};
+
+const desafio = async () => {
+  try {
+    const productManager = new ProductManager("./products.json");
+    await productManager.addProduct({
+      title: "Martillo",
+      description: "Herramienta contundente",
+      price: "1500",
+      thumbnail: "*",
+      code: "1",
+      stock: "5",
+    });
+    await productManager.addProduct({
+      title: "Sierra",
+      description: "Herramienta cortante",
+      price: "750",
+      thumbnail: "*",
+      code: "2",
+      stock: "3",
+    });
+    await productManager.addProduct({
+      title: "Destornillador",
+      description: "Herramienta para desatornillar",
+      price: "1100",
+      thumbnail: "*",
+      code: "3",
+      stock: "5",
+    });
+    await productManager.addProduct({
+      title: "Pinzas",
+      description: "Herramienta cortante",
+      price: "950",
+      thumbnail: "*",
+      code: "1",
+      stock: "4",
+    });
+    const products = await productManager.getProducts();
+    console.log("Acá los productos:", products);
+
+    const productId = 2;
+    await productManager.getProductById(productId);
+
+    await productManager.deleteProduct(productId);
+
+    await productManager.updateProduct(
+      "Motosierra",
+      "Herramienta cortante",
+      "12000",
+      "*",
+      "1",
+      "5",
+      1
+    );
+  } catch (error) {
+    console.error(" Ha ocurrido un error: ", error.message);
+  }
+};
+
+desafio();
+
+module.exports = ProductManager;
---
+++ src/app.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const express = require("express");
+const ProductManager = require("./ProductManager");
+const productos = new ProductManager();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 3000;
+
+function readProductsFromFile() {
+  try {
+    const data = fs.readFileSync("products.json", "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error al leer el archivo products.json:", error);
+    return [];
+  }
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/products", (req, res) => {
+  try {
+    const { limit } = req.query;
+    let products = readProductsFromFile();
+
+    if (limit && !isNaN(parseInt(limit, 10))) {
+      const limitNumber = parseInt(limit, 10);
+      products = products.slice(0, limitNumber);
+    }
+
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
+app.get("/products/:pid", async (req, res) => {
+  try {
+    const productId = req.params.pid;
+    const products = readProductsFromFile();
+
+    const product = products.find((p) => p.id === parseInt(productId, 10));
+
+    if (!product) {
+      res.status(404).json({ error: "Producto no encontrado" });
+    } else {
+      res.json(product);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// La app se puede inicializar usando el comando "npm start" desde la terminal
